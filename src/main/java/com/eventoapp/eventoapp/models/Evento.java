821d9a28d85b7a46a620f2657eb7ab919b30e2e2
MARIOHELIOSIMOES <mariohelio_simoes@hotmail.com>
2022-04-22 18:02:35 -0300

    Aula 3 - conexão com banco de dados

--- src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -1,9 +1,47 @@
 package com.eventoapp.eventoapp.models;
 
-public class Evento {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+
+	
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
 	private String nome, local, data, horario ;
 
+	public Evento() {
+	}
+	
+
+	public Evento(long codigo, String nome, String local, String data, String horario) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.local = local;
+		this.data = data;
+		this.horario = horario;
+	}
+
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
